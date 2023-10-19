5ec9c1afcff9093c8e8e88daf010352926a90cda
심연수 <sim-yeonsu@192-168-1-109.tpgi.com.au>
2023-10-19 23:30:24 +1100

    add function - 'goUp()'

---
+++ im2320/assign_3/control.js
@@ -0,0 +1,43 @@
+/* SET INITIAL POSITION */
+// bring balloon images
+let balloons = document.getElementsByClassName("balloon");
+
+/* SET INITIAL POSITION */
+
+// set each balloon to initial position
+for (let i=0; i<balloons.length; i++) {
+    let amount = 4+i*7;
+    balloons[i].style.left = `${amount}%`;
+    balloons[i].style.zIndex = i;
+}
+
+Array.from(balloons).forEach(balloon => {
+    balloon.addEventListener('click', setZ);
+    balloon.addEventListener('dragstart', setZ);
+    balloon.addEventListener('drag', e => setXY(balloon, e));
+    balloon.addEventListener('dragend', goUp);
+});
+
+function setXY(balloon, e) {
+    let x = e.clientX;
+    let y = e.clientY;
+    if (x==0 && y==0)
+        return;
+
+    balloon.style.left = `${x}px`
+    balloon.style.top = `${y}px`;
+    balloon.style.transition = null;
+    goUp();
+}
+
+function goUp() {
+    let distance = this.style.top.slice(0,-2);
+    let time = distance*10;
+    this.style.transition = "top "+time+"ms ease";
+    this.style.top = "0px";
+}
+
+function setZ() {
+    console.log(this.style.zIndex);
+
+}
