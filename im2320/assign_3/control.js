b1cb76d54e7f712dd4b587068784769d1e6d27a8
심연수 <sim-yeonsu@192-168-1-109.tpgi.com.au>
2023-10-20 03:56:04 +1100

    update function - 'down()'

--- im2320/assign_3/control.js
+++ im2320/assign_3/control.js
@@ -1,8 +1,11 @@
 /* SET INITIAL POSITION */
 // bring balloon images
-let balloons = document.getElementsByClassName("balloon");
+let movable = document.getElementsByClassName("movable");
 
 let isDown = false;
+let isBalloon = false;
+let isCloud = false;
+let isCake = false;
 let x = 0;
 let y = 0;
 let obj;
@@ -10,27 +13,44 @@ let obj;
 /* SET INITIAL POSITION */
 
 // set each balloon to initial position
-for (let i=0; i<balloons.length; i++) {
+for (let i=0; i<movable.length; i++) {
     let amount = 4+i*7;
-    balloons[i].style.left = `${amount}%`;
-    balloons[i].style.zIndex = i;
+    if (i < 13)
+        movable[i].style.left = `${amount}%`;
+    movable[i].style.zIndex = i;
 }
 
 window.onmousedown = function(event) {
-    if (event.target.classList.contains('balloon')) {
-      isDown = true;
-      x = event.offsetX;
-      y = event.offsetY;
-      obj = event.target;
-      setZ();
-      return false;
+    if (event.target.classList.contains("balloon")) {
+        isBalloon = true;
+        isCloud = false;
+        isCake = false;
+        setXY(event);
+        setZ();
+        return false;
+    }
+    else if (event.target.classList.contains("cloud")) {
+        isBalloon = false;
+        isCloud = true;
+        isCake = false;
+        setXY(event);
+        setZ();
+        return false;
+
+    } else if (event.target.classList.contains("cake")) {
+        isBalloon = false;
+        isCloud = false;
+        isCake = true;
+        setXY(event);
+        setZ();
+        return false;
     }
 };
 
 window.onmousemove = function(event) {
     if (isDown) {
-        obj.style.left = event.clientX - x + 'px';
-        obj.style.top  = event.clientY - y + 'px';
+        obj.style.left = event.clientX - x + "px";
+        obj.style.top  = event.clientY - y + "px";
         obj.style.transition = null;
         return false;
     }
@@ -38,23 +58,51 @@ window.onmousemove = function(event) {
 
 window.onmouseup = function(event) {
     isDown = false;
-    goUp();
-  };
+    if (isBalloon)
+        goUp();
+    else if (isCloud)
+        up();
+    else if (isCake)
+        down();
+};
 
-
-function goUp() {
-    let distance = obj.style.top.slice(0,-2);
-    let time = distance*10;
-    obj.style.transition = "top "+time+"ms ease";
-    obj.style.top = "0px";
+function setXY(event) {
+    isDown = true;
+    x = event.offsetX;
+    y = event.offsetY;
+    obj = event.target;
 }
 
 function setZ() {
     let index = obj.style.zIndex;
-    Array.from(balloons).forEach(balloon => {
-        let otherIndex = balloon.style.zIndex;
+    Array.from(movable).forEach(object => {
+        let otherIndex = object.style.zIndex;
         if (otherIndex > index)
-        balloon.style.zIndex = otherIndex-1;
+        object.style.zIndex = otherIndex-1;
     });
-    obj.style.zIndex = balloons.length-1;
+    obj.style.zIndex = movable.length-1;
+}
+
+function goUp() {
+    let top = obj.style.top.slice(0,-2);
+    let time = top*10;
+    obj.style.transition = "top "+time+"ms ease";
+    obj.style.top = "0px";
+}
+
+function up() {
+    let top = obj.style.top.slice(0,-2);
+    obj.style.transition = "top 1s ease";
+    obj.style.top = top - 15 + "px";
+}
+
+function down() {
+    let top = obj.style.top.slice(0,-2);
+    let limit = Math.floor(window.innerHeight*0.35);
+    let distance = limit - top;
+    if (top < limit) {
+        let time = distance*5;
+        obj.style.transition = "top "+time+"ms ease-in";
+        obj.style.top = limit+"px";
+    }
 }
